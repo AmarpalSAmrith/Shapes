1270b5d4f2aa8ff93ef9ca1a48309c86e168e35a
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-05-14 15:18:56 +0100

    Startup

---
+++ src/AmarpalAmrith/TrainingMaterials/Main.java
@@ -0,0 +1,8 @@
+package AmarpalAmrith.TrainingMaterials;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/AmarpalAmrith/TrainingMaterials/Rectangle.java
@@ -0,0 +1,5 @@
+package AmarpalAmrith.TrainingMaterials;
+
+public class Rectangle {
+    
+}
